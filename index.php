46b1faa92985decae2bb1b29258848eb7ef82c89
Jeffrey de Jong <boem-boem2@hotmail.com>
2018-12-13 16:31:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="data/w3.css">
+<link rel="stylesheet" href="style.css">
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<body>
+
+<div class="w3-container w3-green">
+  <h1>Demo responsive</h1> 
+  <p>Resize this page.</p> 
+</div>
+
+<div class="w3-row-padding">
+  <div class="w3-third">
+    <h2>London</h2>
+    <p>London is the capital city of England.</p>
+    <p>It is the most populous city in the United Kingdom,
+    with a metropolitan area of over 13 million inhabitants.</p>
+  </div>
+
+
+  <div class="w3-third">
+    <h2>Paris</h2>
+    <p>Paris is the capital of France.</p> 
+    <p>The Paris area is one of the largest population centers in Europe,
+    with more than 12 million inhabitants.</p>
+  </div>
+
+  <div class="w3-third">
+    <h2>Tokyo</h2>
+
+    <p>Tokyo is the capital of Japan.</p>
+    <p>It is the center of the Greater Tokyo Area,
+    and the most populous metropolitan area in the world.</p>
+  </div>
+  <center>
+  <a href="register.php" class="button">Try to Register...</a>
+  </center>
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+			<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+			<script>
+				$(window).load(function() {
+					$(".se-pre-con").fadeOut("slow");;
+				});
+			</script>
+
+
+		<link href="https://fonts.googleapis.com/css?family=Rokkitt" rel="stylesheet"> 
+		<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	
+        <section class="hero">
+         <div class="container">
+          <div class="row">	
+		  
+		   <div class="col-lg-6 offset-lg-3">
+			
+			<div class="cardbox shadow-lg bg-white">
+			 
+			 <div class="cardbox-heading">
+			  <!-- START dropdown-->
+			  <div class="dropdown float-right">
+			   <button class="btn btn-flat btn-flat-icon" type="button" data-toggle="dropdown" aria-expanded="false">
+				<em class="fa fa-ellipsis-h"></em>
+			   </button>
+			   <div class="dropdown-menu dropdown-scale dropdown-menu-right" role="menu" style="position: absolute; transform: translate3d(-136px, 28px, 0px); top: 0px; left: 0px; will-change: transform;">
+				<a class="dropdown-item" href="#">Hide post</a>
+				<a class="dropdown-item" href="#">Stop following</a>
+				<a class="dropdown-item" href="#">Report</a>
+			   </div>
+			  </div><!--/ dropdown -->
+			  <div class="media m-0">
+			   <div class="d-flex mr-3">
+				<a href=""><img class="img-fluid rounded-circle" src="https://pbs.twimg.com/profile_images/472396850314883073/S378Bcbo_400x400.jpeg" alt="User"></a>
+			   </div>
+			   <div class="media-body">
+			    <p class="m-0">Jeffrey de Jong</p>
+				<small><span><i class="icon ion-md-pin"></i> Drachten, Friesland</span></small>
+				<small><span><i class="icon ion-md-time"></i> 50 minutes ago</span></small>
+			   </div>
+			  </div>
+			 </div>
+			  
+			 <div class="cardbox-item">
+			  <img class="img-fluid" src="data/man.png" alt="Image">
+			 </div>
+			 <div class="cardbox-base">
+			  <ul class="float-right">
+			   <li><a><i class="fa fa-comments"></i></a></li>
+			   <li><a><em class="mr-5">12</em></a></li>
+			   <li><a><i class="fa fa-share-alt"></i></a></li>
+			   <li><a><em class="mr-3">03</em></a></li>
+			  </ul>
+			  <ul>
+			   <li><a><i class="fa fa-thumbs-up"></i></a></li>
+			   <li><a href="#"><img src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/3.jpeg" class="img-fluid rounded-circle" alt="User"></a></li>
+			   <li><a href="#"><img src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/1.jpg" class="img-fluid rounded-circle" alt="User"></a></li>
+			   <li><a href="#"><img src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/5.jpg" class="img-fluid rounded-circle" alt="User"></a></li>
+			   <li><a href="#"><img src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/2.jpg" class="img-fluid rounded-circle" alt="User"></a></li>
+			   <li><a><span>25.4k Likes</span></a></li>
+			  </ul>			   
+			 </div>
+			 <div class="cardbox-comments">
+			  <span class="comment-avatar float-left">
+			   <a href="login.php"><img class="rounded-circle" src="http://www.themashabrand.com/templates/bootsnipp/post/assets/img/users/6.jpg" alt="..."></a>                            
+			  </span>
+			  <div class="search">
+			   <input placeholder="Write a comment" type="text">
+			   <button><i class="fa fa-camera"></i></button>
+			  </div>
+			 </div>		  
+					
+			</div>
+          </div><!--/ row -->
+         </div><!--/ container -->
+        </section>
+</div>
+</body>
+	<div class="se-pre-con"></div>
+</html>
